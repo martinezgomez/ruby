ede60ad6ef4829d85849646dd838f71fc7e6acd7
martinezgomez <enriquemartinez.ciclos@gmail.com>
2015-04-08 13:31:52 +0200

    user

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
 	with: %r{\.(gif|jpg|png)\Z}i,
 	message: 'La url debe ser de un Gif, JPG o PNG.'
 	}
+	
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
